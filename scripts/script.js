9ed07a784a310686b89185e4b952c125ad4df4e2
Keyvan Mahmoudi <iamkeyvan72@Keyvans-iMac.local>
2020-08-05 21:08:06 +0430

    Fix scripts bug in homepage

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,3 @@
-const acc = document.getElementsByClassName("accordion-header");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    const panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
-const showMore = document.getElementById("more-trigger");
-const filmDescription = document.getElementById("film-description");
-showMore.addEventListener("click", (event) => {
-  filmDescription.style.height = "100%";
-  showMore.style.display = "none";
-});
-
 const menuTrigger = document.getElementById("menu-trigger");
 const mobileMenu = document.getElementById("nav-links-sidebar");
 const maskOverlay = document.getElementById("mask");
@@ -32,3 +10,17 @@ maskOverlay.addEventListener("click", (event) => {
   mobileMenu.style.right = "-85%";
   maskOverlay.style.display = "none";
 });
+
+const acc = document.getElementsByClassName("accordion-header");
+let i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
